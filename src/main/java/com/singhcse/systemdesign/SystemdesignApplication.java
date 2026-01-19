51588e137c23154f444b88ae674c9502f8ba68ad
Shubham Singh <singhcse01@gmail.com>
2026-01-19 10:53:38 +0530

    Initial commit with springboot project

---
+++ src/main/java/com/singhcse/systemdesign/SystemdesignApplication.java
@@ -0,0 +1,13 @@
+package com.singhcse.systemdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemdesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemdesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singhcse/systemdesign/SystemdesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singhcse.systemdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemdesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
